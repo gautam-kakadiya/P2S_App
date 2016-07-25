ac2050443c263d39d0352cf2c3e5427d550478f3
gautam-kakadiya <gautam.d.kakadiya@gmail.com>
2016-07-25 19:10:12 +0530

    Added Login View

--- app/src/main/java/com/utils/gdkcorp/p2sapp/LoginActivity.java
+++ app/src/main/java/com/utils/gdkcorp/p2sapp/LoginActivity.java
@@ -1,13 +1,38 @@
 package com.utils.gdkcorp.p2sapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private EditText uname,pswd;
+    private Button login_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        uname = (EditText) findViewById(R.id.user_name);
+        pswd = (EditText) findViewById(R.id.password);
+        login_button = (Button) findViewById(R.id.login_button);
+        login_button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(!uname.getText().toString().equalsIgnoreCase("") && !pswd.getText().toString().equalsIgnoreCase("")){
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+        }else {
+            if (uname.getText().toString().equalsIgnoreCase("")) {
+                uname.setError("Username can't be blank");
+            }
+            if (pswd.getText().toString().equalsIgnoreCase("")) {
+                pswd.setError("Password can't be blank");
+            }
+        }
     }
 }
